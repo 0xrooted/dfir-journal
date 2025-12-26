e48fd767ac9b3f9e3267001f8ff4b415445f3da0
Root <upanshuchoudhary18@gmail.com>
2025-12-26 21:05:19 +0530

    Add IP extraction and user activity logging script

---
+++ scripts/ip_extractor.py
@@ -0,0 +1,8 @@
+input_file="sample.log"
+output_file= "extracted_ip.log"
+with open(input_file, "r") as infile:
+    with open(output_file, "w") as outfile:
+        for line in infile:
+            if "IP:" in line:
+                outfile.write(line)
+print("Ip Extraction complete")
---
+++ scripts/user_activity.py
@@ -0,0 +1,18 @@
+username = input("Enter the Username: ")
+input_file = "sample.log"
+output_file= f"user_{username}_logs.log"
+
+found = False
+
+with open (input_file,"r") as infile:
+    with open(output_file,"w") as outfile:
+        for line in infile:
+            if f"user={username}" in line:
+                outfile.write(line)
+
+found = True
+
+if found:
+    print(f"Logs saved for user: {username}")
+else:
+    print("User didn't exist")
